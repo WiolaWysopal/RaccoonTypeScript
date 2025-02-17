47e30fbe8ff7b3f3e04dcf8dc6ab5e4886e6a12a
wwysopal <wioletta.wysopal@gmail.com>
2025-02-17 10:58:04 +0100

    Modify TSvsJS.js file

--- IntroductionAndBasicSetupTS/TSvsJS.js
+++ IntroductionAndBasicSetupTS/TSvsJS.js
@@ -1,23 +1,26 @@
-var array = [1, 2, 3.4];
-var array2 = ["ts", "js", "let", "var", "const"];
-function calculateNumeric(arr) {
-    var sum = 0.0;
-    if (!Array.isArray(arr)) {
+var array = [1, 2, 3.4, -12n];
+var array2 = ["ts", "js", "let", "var", "const"]
+function calculateNumeric(arr)
+{
+    let sum = 0.0;
+    if (!Array.isArray(arr))
+    {
         throw new Error("ERROR: INPUT IS NOT ARRAY");
+        
     }
-    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-        var a = arr_1[_i];
+    for (let a of arr)
+    {
         if (typeof a === "number")
             sum += a;
         else if (typeof a === "bigint")
             sum += Number(a);
         else
-            // in TS + with string concatenates string elements together;
-            // to simulate an error situation it's neccessary to create 
-            // an error situation which can be * instead of +
+    // in JS + with string concatenates string elements together
+    // to simulate an error situation it's neccessary to create 
+    // an error situation which can be * instead of +
             sum *= a;
     }
     return sum;
 }
-console.log("ARRAY: Sum of number elements: ".concat(calculateNumeric(array)));
-//console.log(`ARRAY2: Sum of number elements: ${calculateNumeric(array2)}`);
+console.log(`ARRAY: Sum of number elements: ${calculateNumeric(array)}`);
+console.log(`ARRAY2: Sum of number elements: ${calculateNumeric(array2)}`);
